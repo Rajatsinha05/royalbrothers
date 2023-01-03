7d60150418f54958ad2b32c4630af3d8bcbce28d
pradipk1 <pradipshukla12699@gmail.com>
2023-01-03 20:06:57 +0530

    project started

--- src/AllPages/Rbxsubscription.jsx
+++ src/AllPages/Rbxsubscription.jsx
@@ -1,8 +1,98 @@
 import React from 'react'
+import './Rbxsubscription.css'
 
 function Rbxsubscription() {
   return (
-    <div>Rbxsubscription</div>
+    <div>
+      <section className='hero-pk'></section>
+      {/* <section className='hero-mobile-pk'>
+        <div className='hero-mobile-card-pk'>
+          <h3 className='hero-mobile-card-heading-pk'>DELIVERY DATE</h3>
+          <div className='hero-mobile-card-datetime-parent-pk'>
+            <div className='hero-mobile-card-datetime-child-pk'>
+              <div className='input-container-pk'>
+                <input type='date'></input>
+              </div>
+              <div className='input-container-pk'>
+                <select className='pickup-time-select-pk'>
+                  <option value="">Select time</option>
+                  <option value=""></option>
+                  <option value=""></option>
+                  <option value=""></option>
+                  <option value=""></option>
+                  <option value=""></option>
+                  <option value=""></option>
+                  <option value=""></option>
+                  <option value=""></option>
+                  <option value=""></option>
+                  <option value=""></option>
+                  <option value=""></option>
+                  <option value=""></option>
+                  <option value=""></option>
+                  <option value=""></option>
+                  <option value=""></option>
+                  <option value=""></option>
+                  <option value=""></option>
+                  <option value=""></option>
+                  <option value=""></option>
+                </select>
+              </div>
+            </div>
+            <div className='hero-mobile-card-datetime-child-pk'>
+              <button className='hero-mobile-card-btn-pk'>SEARCH</button>
+            </div>
+          </div>
+        </div>
+      </section> */}
+      <section className='hit-pk'>
+        <h3 className='hiw-heading-pk'>How it Works</h3>
+        <div className='hiw-flex-pk'>
+          <div className='hiw-items-pk'>
+            <img className='hiw-image-pk' src="https://d36g7qg6pk2cm7.cloudfront.net/assets/long_term/hiw_scooter-340840f2aa3263cef93a01a80ae023a2022c50e9b0294492bc991348859c82ec.png" alt="" />
+            <p className='hiw-desc-pk'>Select Your Ride</p>
+            <img className='hiw-image-arrow-pk' src="https://d36g7qg6pk2cm7.cloudfront.net/assets/long_term/hiw_arrow-6ba7ec172cde95d071dc770fc029f7639c295b072adea3c9a46c6748445f3f39.png" alt="" />
+          </div>
+          <div className='hiw-items-pk'>
+            <img className='hiw-image-pk' src="https://d36g7qg6pk2cm7.cloudfront.net/assets/long_term/hiw_deposit-a2db02190834c271e9cbe48e0f26e22e6099769c9ade8886ba6fb4e44a6ffc98.png" alt="" />
+            <p className='hiw-desc-pk'>
+              Pay deposite & booking 
+              <br />
+              amount for first month
+            </p>
+            <img className='hiw-image-arrow-pk' src="https://d36g7qg6pk2cm7.cloudfront.net/assets/long_term/hiw_arrow-6ba7ec172cde95d071dc770fc029f7639c295b072adea3c9a46c6748445f3f39.png" alt="" />
+          </div>
+          <div className='hiw-items-pk'>
+            <img className='hiw-image-pk' src="https://d36g7qg6pk2cm7.cloudfront.net/assets/long_term/hiw_kyc-8422dfb7da6bac4e7bbc8ef8908c3a8063fb02a4c5a7a95aad15a9b11a4f1150.png" alt="" />
+            <p className='hiw-desc-pk'>
+              Get KYC approved online 
+              <br />
+              & start ridind
+            </p>
+            <img className='hiw-image-arrow-pk' src="https://d36g7qg6pk2cm7.cloudfront.net/assets/long_term/hiw_arrow-6ba7ec172cde95d071dc770fc029f7639c295b072adea3c9a46c6748445f3f39.png" alt="" />
+          </div>
+          <div className='hiw-items-pk'>
+            <img className='hiw-image-pk' src="https://d36g7qg6pk2cm7.cloudfront.net/assets/long_term/hiw_rent-ba691edeb88c3c0f8ffb1b70148122e48b37d148f421511383c07e2387cf5a5e.png" alt="" />
+            <p className='hiw-desc-pk'>
+              Pay for upcoming 
+              <br />
+              months at the start 
+              <br />
+              of every month
+            </p>
+            <img className='hiw-image-arrow-pk' src="https://d36g7qg6pk2cm7.cloudfront.net/assets/long_term/hiw_arrow-6ba7ec172cde95d071dc770fc029f7639c295b072adea3c9a46c6748445f3f39.png" alt="" />
+          </div>
+          <div className='hiw-items-pk'>
+            <img className='hiw-image-pk' src="https://d36g7qg6pk2cm7.cloudfront.net/assets/long_term/hiw_exchange-ddece91b095998b3b4783edb0f53c65431350248da8ee437b58843e647e961fa.png" alt="" />
+            <p className='hiw-desc-pk'>
+              Our team will service the 
+              <br />
+              vehicle periodically at no cost
+            </p>
+            {/* <img className='hiw-image-arrow-pk' src="https://d36g7qg6pk2cm7.cloudfront.net/assets/long_term/hiw_arrow-6ba7ec172cde95d071dc770fc029f7639c295b072adea3c9a46c6748445f3f39.png" alt="" /> */}
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
 
