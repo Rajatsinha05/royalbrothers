d314b652186c4e5983f169fdb4471613f1e0fe0e
sanketsapatevnit@gmail.com <sanketsapatevnit@gmail.com>
2023-01-04 20:28:18 +0530

    New

--- src/AllPages/Cart.jsx
+++ src/AllPages/Cart.jsx
@@ -1,8 +1,29 @@
+import { Box, Center, Flex,Text } from '@chakra-ui/react'
 import React from 'react'
 
 function Cart() {
+  let style ={
+    bRadius: '10px',
+    p: '10px'
+  }
   return (
-    <div>Cart</div>
+    <>
+    <Box w='100%' bgColor='#fed250'>
+      <Text fontWeight='500' maxW='80%' align='center' p='2px'>
+      Now rent two wheelers for 4 or 7 days at a lower special price. Click to modify search to 4 days or 7 days ( 7 Hours and 30 Minutes )
+      </Text>
+    </Box>
+    <Center>
+      <Flex>
+        <Box boxShadow='0 0 10px black' borderRadius={style.bRadius}>
+          <Text fontWeight='700' p='5px'>Summary</Text>
+        </Box>
+        <Box boxShadow='0 0 10px black' borderRadius={style.bRadius}>
+          1
+        </Box>
+      </Flex>
+    </ Center>
+    </>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ import './index.css';
 import App from './App';
 
 import {BrowserRouter} from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
-    <BrowserRouter>
-   
-    <App />
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
 
