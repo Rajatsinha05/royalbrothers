34b0248d72390bf3e0343b99ca6f4b19f6882e26
Rajat Sinha <111138210+Rajatsinha05@users.noreply.github.com>
2023-01-05 19:34:36 +0530

    Update Product.jsx

--- src/AllPages/Product.jsx
+++ src/AllPages/Product.jsx
@@ -1,10 +1,15 @@
+
 import React from 'react'
+import Services from '../Components/Services'
+import Container from '../Components/Container'
 
 function Product() {
   return (
-   <>
-    
-    </>
+   <div>
+   
+   
+   <Container/>
+   </div>
   )
 }
 
