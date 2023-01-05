e9848ce2bb57464725649e6148ea29c1630b408d
adityarajbenn <adityarajbenn@gmail.com>
2023-01-05 12:13:34 +0530

    added Loader, quality component improvement

--- src/AllPages/Home.jsx
+++ src/AllPages/Home.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import DateSelector from "../Components/Home/DateSelector";
 import FeaturedIn from "../Components/Home/FeaturedIn";
 import OurFleet from "../Components/Home/OurFleet";
+import PopupCard from "../Components/Home/PopupCard";
 import Quality from "../Components/Home/Quality";
 import NavBar from "../Components/NavBar";
 
 function Home() {
+  const [show,setShow] = useState(true);
+  function closePopup(data){
+    setShow(data);
+  }
   return (
     <>
       <NavBar />
       <DateSelector />
       <OurFleet/>
       <Quality/>
+      {
+        show? <PopupCard closePopup={closePopup}/> : ""
+      }
+      
     </>
   );
 }
